d2cabf5b6e51b9d2ac88d0e92e1f4ef34a787abb
huang-annie <10835161@fhsh.tp.edu.tw>
2022-03-03 14:54:42 +0800

    init project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const path = require("path");
+
+const mdFile = ".md";
+const rootFolder = path.dirname(__dirname);
+const rootfiles = fs.readdirSync(rootFolder);
+const folders = rootfiles.filter(
+  (item) => path.extname(item) != mdFile && item != ".vuepress"
+);
+
+module.exports = {
+  title: "Annie's  notes",
+  description: "這是我在上課的筆記紀錄",
+  themeConfig: {
+    logo: "https://e.ecimg.tw/items/DEASABA900A1IPI/i010004_1559023803.jpg",
+    navbar: [...getNavBar()],
+    sidebar: { ...getSideBar() },
+  },
+};
+
+function getNavBar() {
+  const navbar = [];
+  folders.forEach((folder) => {
+    navbar.push({
+      text: folder.toUpperCase(),
+      link: `/${folder}/`,
+    });
+  });
+  return navbar;
+}
+
+function getSideBar() {
+  const sidebar = {};
+  folders.forEach((folder) => {
+    sidebar[`/${folder}/`] = [];
+    const folderFiles = fs.readdirSync(path.join(rootFolder, folder));
+    const children = [];
+    folderFiles
+      .filter((item) => item.toLowerCase() != "readme.md")
+      .forEach((file) => {
+        children.push(`/${folder}/${file}`);
+      });
+    sidebar[`/${folder}/`].push({
+      text: folder.toUpperCase(),
+      children: [`/${folder}/`, ...children],
+    });
+  });
+  return sidebar;
+}
---
+++ docs/README.md
@@ -0,0 +1,10 @@
+---
+home: true
+heroImage: https://e.ecimg.tw/items/DEASABA900A1IPI/i010004_1559023803.jpg
+heroText: My Notes
+tagline: 快速、簡便、容易運作！
+actions:
+  - text: 快速上手
+    link: /blog/
+    type: primary
+---
